ffd36f567d3cf41847fd8258c0e99095671b98da
Beliz Yilmaz <belizbel@live.unc.edu>
2019-11-06 22:11:57 -0500

    Made another important edit

--- Eventz/Eventz/AppDelegate.swift
+++ Eventz/Eventz/AppDelegate.swift
@@ -11,6 +11,7 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    // max nabokow sucks
     // beliz yilmaz sucks
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
