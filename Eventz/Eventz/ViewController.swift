bbfbc214e87fde849839e287167b27d594b9c48f
Max Nabokow <max.nabokow@gmail.com>
2019-11-06 22:08:18 -0500

    Made very important change

---
+++ Eventz/Eventz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Eventz
+//
+//  Created by Max Nabokow on 11/6/19.
+//  Copyright © 2019 Maximilian Nabokow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
