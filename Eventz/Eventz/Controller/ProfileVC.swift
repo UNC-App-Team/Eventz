90a2607af994c85c7c2c691bc76634e499b7a82f
Max Nabokow <max.nabokow@gmail.com>
2019-12-23 19:46:02 +0100

    Added penButton and myInterestsLabel

--- Eventz/Eventz/Controller/ProfileVC.swift
+++ Eventz/Eventz/Controller/ProfileVC.swift
@@ -35,18 +35,29 @@ class ProfileViewController: UIViewController {
         return v
     }()
     
+    fileprivate let penButton: UIButton = {
+        let b = UIButton(type: .system)
+        b.setImage(#imageLiteral(resourceName: "pencil"), for: .normal)
+        return b
+    }()
+    
     fileprivate let nameLabel: UILabel = {
-        let l = UILabel(text: "Maria Snow", font: .systemFont(ofSize: 32, weight: .bold), textColor: .black, textAlignment: .left, numberOfLines: 1)
+        let l = UILabel(text: "Maria Snow", font: .systemFont(ofSize: 32, weight: .bold), textColor: .black, textAlignment: .left, numberOfLines: 0)
         return l
     }()
     
     fileprivate let infoLabel: UILabel = {
-        let l = UILabel(text: "San Francisco, CA \nUNC \n20 years", font: .systemFont(ofSize: 16, weight: .regular), textColor: .darkGray, textAlignment: .left, numberOfLines: 3)
+        let l = UILabel(text: "San Francisco, CA \nUNC \n20 years", font: .systemFont(ofSize: 16, weight: .regular), textColor: .darkGray, textAlignment: .left, numberOfLines: 0)
         return l
     }()
     
     fileprivate let majorsLabel: UILabel = {
-        let l = UILabel(text: "Majors: \n- Anthropology \n- Computer Science", font: .systemFont(ofSize: 16, weight: .regular), textColor: .darkGray, textAlignment: .left, numberOfLines: 3)
+        let l = UILabel(text: "Majors: \n- Anthropology \n- Computer Science", font: .systemFont(ofSize: 16, weight: .regular), textColor: .darkGray, textAlignment: .left, numberOfLines: 0)
+        return l
+    }()
+    
+    fileprivate let myInterestsLabel: UILabel = {
+        let l = UILabel(text: "My Interests", font: .systemFont(ofSize: 26, weight: .medium), textColor: .black, textAlignment: .left, numberOfLines: 1)
         return l
     }()
     
@@ -75,27 +86,33 @@ class ProfileViewController: UIViewController {
     
     fileprivate func setupLayout() {
         view.addSubview(profilePhotoBackgroundView)
-        profilePhotoBackgroundView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32).isActive = true
-        profilePhotoBackgroundView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.25).isActive = true
+        profilePhotoBackgroundView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 24).isActive = true
+        profilePhotoBackgroundView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.20).isActive = true
         profilePhotoBackgroundView.widthAnchor.constraint(equalTo: profilePhotoBackgroundView.heightAnchor).isActive = true
         profilePhotoBackgroundView.centerXToSuperview()
         
         view.addSubview(profilePhotoImageView)
-        profilePhotoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32).isActive = true
-        profilePhotoImageView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.25).isActive = true
-        profilePhotoImageView.widthAnchor.constraint(equalTo: profilePhotoImageView.heightAnchor).isActive = true
+        profilePhotoImageView.topAnchor.constraint(equalTo: profilePhotoBackgroundView.topAnchor).isActive = true
+        profilePhotoImageView.heightAnchor.constraint(equalTo: profilePhotoBackgroundView.heightAnchor).isActive = true
+        profilePhotoImageView.widthAnchor.constraint(equalTo: profilePhotoBackgroundView.widthAnchor).isActive = true
         profilePhotoImageView.centerXToSuperview()
         
         view.addSubview(bottomView)
-        bottomView.anchor(top: profilePhotoImageView.bottomAnchor, leading: view.leadingAnchor, bottom: view.bottomAnchor, trailing: view.trailingAnchor, padding: .init(top: 50, left: 0, bottom: 0, right: 0))
+        bottomView.anchor(top: profilePhotoImageView.bottomAnchor, leading: view.leadingAnchor, bottom: view.bottomAnchor, trailing: view.trailingAnchor, padding: .init(top: 32, left: 0, bottom: 0, right: 0))
+        
+        view.addSubview(penButton)
+        penButton.anchor(top: view.topAnchor, leading: nil, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 32, left: 0, bottom: 0, right: 18), size: .init(width: 25, height: 25))
         
         bottomView.addSubview(nameLabel)
-        nameLabel.anchor(top: bottomView.topAnchor, leading: bottomView.leadingAnchor, bottom: nil, trailing: nil, padding: .init(top: 24, left: 24, bottom: 0, right: 0))
+        nameLabel.anchor(top: bottomView.topAnchor, leading: bottomView.leadingAnchor, bottom: nil, trailing: penButton.leadingAnchor, padding: .init(top: 24, left: 24, bottom: 0, right: 16))
         
         bottomView.addSubview(infoLabel)
         infoLabel.anchor(top: nameLabel.bottomAnchor, leading: nameLabel.leadingAnchor, bottom: nil, trailing: bottomView.centerXAnchor, padding: .init(top: 16, left: 0, bottom: 0, right: 4))
         
         bottomView.addSubview(majorsLabel)
         majorsLabel.anchor(top: infoLabel.topAnchor, leading: bottomView.centerXAnchor, bottom: nil, trailing: bottomView.trailingAnchor, padding: .init(top: 0, left: 4, bottom: 0, right: 24))
+        
+        bottomView.addSubview(myInterestsLabel)
+        myInterestsLabel.anchor(top: infoLabel.bottomAnchor, leading: nameLabel.leadingAnchor, bottom: nil, trailing: nil, padding: .init(top: 24, left: 0, bottom: 0, right: 0))
     }
 }
