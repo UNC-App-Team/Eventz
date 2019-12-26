50dd6fa35b6a531e054af785def7f73ac4605001
Max Nabokow <max.nabokow@gmail.com>
2019-12-26 19:24:45 +0100

    Added another potential Profile screen

---
+++ Eventz/Eventz/Controller/Profile/SetupProfileVC.swift
@@ -0,0 +1,47 @@
+//
+//  SetupProfileVC.swift
+//  Eventz
+//
+//  Created by Max Nabokow on 12/25/19.
+//  Copyright © 2019 Maximilian Nabokow. All rights reserved.
+//
+
+import UIKit
+
+class SetupProfileViewController: UIViewController {
+    
+    // MARK: - UI Elements
+    
+    
+    
+    // MARK: - Properties
+    
+    
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupUI()
+        setupLayout()
+    }
+    
+    // MARK: - UI Setup
+    
+    fileprivate func setupUI() {
+        
+    }
+    
+    fileprivate func setupLayout() {
+        
+    }
+    
+    // MARK: - Selectors
+    
+    
+    
+    // MARK: - Helper Functions
+    
+    
+}
